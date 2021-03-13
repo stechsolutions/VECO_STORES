21c7901c2d3972a42943ced0883530b07ba63cad
AshharImam <ashharimam490@gmail.com>
2021-03-13 11:08:55 +0500

    pushing

--- android/app/src/main/java/com/app/MainApplication.java
+++ android/app/src/main/java/com/app/MainApplication.java
@@ -14,6 +14,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import com.airbnb.android.react.maps.MapsPackage;
 import java.util.List;
+// import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MapsPackage());
+          //  packages.add(new ReactNativePushNotificationPackage())
           return packages;
         }
 
